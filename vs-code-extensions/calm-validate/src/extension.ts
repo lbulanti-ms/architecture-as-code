c136239cb32796cc670894d7cc34195a39b14db6
Graham Packer <graham.packer@morganstanley.com>
2024-10-07 14:08:31 +0100

    #442 - Add diagnostics when running calm validate extention

--- vs-code-extensions/calm-validate/src/extension.ts
+++ vs-code-extensions/calm-validate/src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { stringify } from 'querystring';
 import * as vscode from 'vscode';
+import { languages, Diagnostic, DiagnosticSeverity, Range } from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,14 +15,52 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('calm-validate.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('calm-validate.calm-validate', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from calm-validate!');
+
+		let calmResult = callCalmValidate();
+
+		let diagnostics: Diagnostic[] = calmResult.map((calmValidation: any) => new Diagnostic(new Range(new vscode.Position(1,1),new vscode.Position(1,1)),
+		calmValidation.message, convertSeverity(calmValidation.severity)));
+
+		let diagnosticCollection = languages.createDiagnosticCollection("calm");
+	
+		if (vscode.window.activeTextEditor) {
+			diagnosticCollection.set(vscode.window.activeTextEditor.document.uri, diagnostics);
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+function callCalmValidate() {
+	return JSON.parse(`[
+    {
+        "code": "json-schema",
+        "severity": "error",
+        "message": "schema is invalid: data/properties/relationships/minItems must be >= 0",
+        "path": "/"
+    },
+    {
+        "code": "pattern-has-no-placeholder-properties-numerical",
+        "severity": "warning",
+        "message": "Numerical placeholder (-1) detected in instantiated pattern.",
+        "path": "/properties/relationships/minItems"
+    }
+]`);
+}
+
+function convertSeverity(severityStr: string): DiagnosticSeverity {
+	if(severityStr === "error") {
+		return DiagnosticSeverity.Error;
+	} else if (severityStr === "warning") {
+		return DiagnosticSeverity.Warning;
+	} else {
+		return DiagnosticSeverity.Information;
+	}
+}
+
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
